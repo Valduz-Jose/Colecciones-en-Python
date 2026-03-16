ee5486bc4c0f87bccadd0a5d0b375c59824b77b3
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-16 16:13:59 -0400

    Eliminar canciones predeterminadas y permitir al usuario ingresar su propia lista de reproducción

--- lista_reproduccion.py
+++ lista_reproduccion.py
@@ -3,10 +3,14 @@
 print("*****Playlist de canciones*****")
 
 lista_reproduccion = []
-lista_reproduccion.append("Bohemian Rhapsody - Queen")
-lista_reproduccion.append("Stairway to Heaven - Led Zeppelin")
-lista_reproduccion.append("Hotel California - Eagles")
-lista_reproduccion.append("Imagine - John Lennon")
+# lista_reproduccion.append("Bohemian Rhapsody - Queen")
+# lista_reproduccion.append("Stairway to Heaven - Led Zeppelin")
+# lista_reproduccion.append("Hotel California - Eagles")
+# lista_reproduccion.append("Imagine - John Lennon")
+numero_canciones = int(input("¿Cuántas canciones deseas agregar a tu lista de reproducción? "))
+for i in range(numero_canciones):
+    cancion = input(f"Ingresa el nombre de la canción {i+1}: ")
+    lista_reproduccion.append(cancion)
 
 # Ordenar en orden alfabetico
 lista_reproduccion.sort()
