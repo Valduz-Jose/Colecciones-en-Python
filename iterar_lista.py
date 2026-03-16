fd3f2bef5906b91bb5fef24634f6fa4372f384db
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-16 15:54:47 -0400

    Agregar script para iterar sobre listas en Python

---
+++ iterar_lista.py
@@ -0,0 +1,11 @@
+print("Iterar Listas")
+nombres = ["Alice", "Bob", "Charlie", "David"]
+
+for nombre in nombres:
+    print(nombre)
+    
+lista_heterogenea = [1, "hola", 3.14, True]
+
+print()
+for elemento in lista_heterogenea:
+    print(elemento)
