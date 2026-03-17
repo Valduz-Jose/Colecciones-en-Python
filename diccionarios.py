6b3910283bb7927f17d7eb1cfb7c7883ecaca5ae
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-17 10:02:11 -0400

    Agregar ejemplos de modificación y acceso a elementos en diccionarios

--- diccionarios.py
+++ diccionarios.py
@@ -14,3 +14,33 @@ print(f"Diccionario Original : {persona}")
 print(f"Nombre: {persona['nombre']}")
 print(f"Edad: {persona.get('edad')}")
 print(f"Ciudad: {persona.get('ciudad')}")
+
+# Modificar el valor de un diccionario
+persona['edad'] = 31
+print(f"Diccionario Modificado : {persona}")
+
+# agregar un nuevo elemento
+persona['profesion'] = 'Ingeniero'
+print(f"Diccionario Modificado : {persona}")
+
+# eliminar un elemento
+del persona['ciudad']
+print(f"Diccionario Modificado : {persona}")
+
+# usndo pop
+persona.pop('profesion')
+print(f"Diccionario Modificado : {persona}")
+
+# iterar los elementos
+print("Iterando sobre el diccionario:")
+for clave, valor in persona.items():
+    print(f"{clave}: {valor}")
+    
+# metodo values y metodo keys
+print("\n Valores del diccionario:")
+for valor in persona.values():
+    print(f"Valor: {valor}")
+    
+print("\n Llaves del diccionario:")
+for llave in persona.keys():
+    print(f"llave: {llave}")
