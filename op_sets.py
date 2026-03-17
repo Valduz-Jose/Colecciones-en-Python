485fca05e33bc1c697d1ea9b195ed1cc4aafbdb0
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-17 09:38:24 -0400

    Agregar script para realizar operaciones con sets en Python

---
+++ op_sets.py
@@ -0,0 +1,14 @@
+print("Operaciones con tipo sets")
+a = {1,2,3,4}
+b = {3,4,5,6}
+
+# Union de sets 
+union = a | b
+print(f"Union de a y b: {union}")
+
+# Interseccion
+interseccion = a & b
+print(f"Interseccion de a y b: {interseccion}")
+# diferencia
+diferencia = a - b
+print(f"Diferencia de a y b: {diferencia}")
